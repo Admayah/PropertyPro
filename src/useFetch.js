c1aa88419836c1bc8588d2e2162260bee10be791
admaya <mayobami49@gmail.com>
2022-08-24 10:43:19 +0100

    made some changes

--- src/useFetch.js
+++ src/useFetch.js
@@ -8,6 +8,7 @@ const [loading, setLoading] = useState(true)
     const getProperties = async () => {
         const response = await axios.get(`${process.env.REACT_APP_BASEURL}/properties`)
         const {data} = response
+        console.log(data)
         setDatas(paginate(data))
         setLoading(false)
     }
