609b88df964a70273743c54f5e9d4c83bccb9a95
admaya <mayobami49@gmail.com>
2022-08-26 11:56:04 +0100

    made changes to files

--- src/components/editProperty/EditProperty.jsx
+++ src/components/editProperty/EditProperty.jsx
@@ -14,9 +14,9 @@ import { useEffect } from 'react'
 
 const EditProperty = ({data}) => {
 
-  const initialValues = {
+  // const initialValues = {
 
-  }
+  // }
 
 
   
@@ -45,24 +45,25 @@ const EditProperty = ({data}) => {
       console.log(datas)
     }, [])
 
+    console.log(datas[0])
 
-const { image_url, title, address, state, land_area, purpose, description, year_of_build, price, no_of_bathrooms, no_of_rooms, no_of_store, no_of_garage } = datas[0];
+// const { image_url, title, address, state, land_area, purpose, description, year_of_build, price, no_of_bathrooms, no_of_rooms, no_of_store, no_of_garage } = datas;
 
-const [editProperty, setEditProperty] = useState({ 
-  image_url, 
-  title, 
-  address, 
-  state, 
-  land_area, 
-  purpose, 
-  description, 
-  year_of_build, 
-  price, 
-  no_of_bathrooms, 
-  no_of_rooms, 
-  no_of_store, 
-  no_of_garage
-});
+// const [editProperty, setEditProperty] = useState({ 
+//   image_url, 
+//   title, 
+//   address, 
+//   state, 
+//   land_area, 
+//   purpose, 
+//   description, 
+//   year_of_build, 
+//   price, 
+//   no_of_bathrooms, 
+//   no_of_rooms, 
+//   no_of_store, 
+//   no_of_garage
+// });
   
 
   const editHandler = (e) => {
