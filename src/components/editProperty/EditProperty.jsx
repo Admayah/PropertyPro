63b55679e4f84547a852398268cab1bcd95bf65f
admaya <mayobami49@gmail.com>
2022-08-26 13:19:48 +0100

    made changes to files

--- src/components/editProperty/EditProperty.jsx
+++ src/components/editProperty/EditProperty.jsx
@@ -45,9 +45,11 @@ const EditProperty = ({data}) => {
       console.log(datas)
     }, [])
 
-    console.log(datas)
+    // console.log(datas)
 
-// const { image_url, title, address, state, land_area, purpose, description, year_of_build, price, no_of_bathrooms, no_of_rooms, no_of_store, no_of_garage } = datas;
+const { image_url, title, address, state, land_area, purpose, description, year_of_build, price, no_of_bathrooms, no_of_rooms, no_of_store, no_of_garage } = datas;
+
+console.log(image_url, title, address, state, land_area, purpose, description, year_of_build, price, no_of_bathrooms, no_of_rooms, no_of_store, no_of_garage)
 
 const [editProperty, setEditProperty] = useState({})
 
