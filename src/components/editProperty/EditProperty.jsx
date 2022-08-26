30da0c967122f0530bbd7e390e5060d91b3fd583
admaya <mayobami49@gmail.com>
2022-08-26 16:45:44 +0100

    made changes to files

--- src/components/editProperty/EditProperty.jsx
+++ src/components/editProperty/EditProperty.jsx
@@ -48,7 +48,7 @@ const EditProperty = ({data}) => {
 
     // console.log(datas)
 
-const {  title, address, state, land_area, purpose, description, year_of_build, price, no_of_bathrooms, no_of_rooms, no_of_store, no_of_garage } = datas[0];
+const {  title, address, state, land_area, purpose, description, year_of_build, price, no_of_bathrooms, no_of_rooms, no_of_store, no_of_garage } = datas;
 
 console.log( title, address, state, land_area, purpose, description, year_of_build, price, no_of_bathrooms, no_of_rooms, no_of_store, no_of_garage)
 
