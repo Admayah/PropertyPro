e72232e2a879441bdf73ea0fd8a618d60dedcd81
admaya <mayobami49@gmail.com>
2022-08-30 13:01:57 +0100

    add a conditional statement

--- src/components/allproperties/AllPropertiesData.jsx
+++ src/components/allproperties/AllPropertiesData.jsx
@@ -17,7 +17,9 @@ const {loading, datas} = useFetch();
 
   useEffect(() => {
     const newData = async () => { 
+      console.log('paginate', datas[page])
       const check = await datas[page]
+      console.log('check======>', check)
       setProperties(check)
     }
    newData()
