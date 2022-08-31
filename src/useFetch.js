50a8edeff9dd68e62114ba0ef7c90abcb692ebee
admaya <mayobami49@gmail.com>
2022-08-31 11:26:49 +0100

    worked on file changes

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,42 +1,42 @@
-// import { useState, useEffect } from "react";
-// import paginate from "./utils";
-// import axios from "axios";
-
-// export const useFetch = () => {
-// const [loading, setLoading] = useState(true)
-//     const [datas, setDatas] = useState();
-//     const getProperties = async () => {
-//         const response = await axios.get(`${process.env.REACT_APP_BASEURL}/properties`)
-//         const {data} = response
-//         console.log(data)
-//         setDatas(paginate(data))
-//         setLoading(false)
-//     }
-
-//     useEffect(() => {
-//         getProperties()
-//     }, [])
-// return {loading, datas}
-// }
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
+import paginate from "./utils";
 import axios from "axios";
 
 export const useFetch = () => {
-    const [post, setPost] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const [currentPage, setCurrentPage] = useState(1);
+const [loading, setLoading] = useState(true)
+    const [datas, setDatas] = useState();
+    const getProperties = async () => {
+        const response = await axios.get(`${process.env.REACT_APP_BASEURL}/properties`)
+        const {data} = response
+        console.log(data)
+        setDatas(paginate(data))
+        setLoading(false)
+    }
 
     useEffect(() => {
-        const getProperties = async () => {
-            const response = await axios.get(`${process.env.REACT_APP_BASEURL}/properties`)
-            const { data } = response
-            console.log(data)
-            setPost(data)
-            // setDatas(paginate(data))
-            setLoading(false)
-        }
-
-        getProperties();
+        getProperties()
     }, [])
-    console.log(post)
+return {loading, datas}
 }
+// import React, { useState, useEffect } from "react";
+// import axios from "axios";
+
+// export const useFetch = () => {
+//     const [post, setPost] = useState([]);
+//     const [loading, setLoading] = useState(false);
+//     const [currentPage, setCurrentPage] = useState(1);
+
+//     useEffect(() => {
+//         const getProperties = async () => {
+//             const response = await axios.get(`${process.env.REACT_APP_BASEURL}/properties`)
+//             const { data } = response
+//             console.log(data)
+//             setPost(data)
+//             // setDatas(paginate(data))
+//             setLoading(false)
+//         }
+
+//         getProperties();
+//     }, [])
+//     console.log(post)
+// }
