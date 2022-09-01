a9b4caa187a939e745010eb9421067c2bfbc0f92
admaya <mayobami49@gmail.com>
2022-09-01 10:49:25 +0100

    get property data from db to be edited

--- src/components/editProperty/EditProperty.jsx
+++ src/components/editProperty/EditProperty.jsx
@@ -43,21 +43,21 @@ const EditProperty = () => {
       
     }, [])
 
-// const {  title, address, state, land_area, purpose, description, year_of_build, price, no_of_bathrooms, no_of_rooms, no_of_store, no_of_garage } = datas;
+const {  title, address, state, land_area, purpose, description, year_of_build, price, no_of_bathrooms, no_of_rooms, no_of_store, no_of_garage } = getData[0];
 
 const [editProperty, setEditProperty] = useState({ 
-  title: "", 
-  address: "", 
-  state: "", 
-  land_area: "", 
-  purpose: "", 
-  description: "", 
-  year_of_build: "", 
-  price: "", 
-  no_of_bathrooms: "", 
-  no_of_rooms: "", 
-  no_of_store: "",
-  no_of_garage: ""
+  title,
+  address,
+  state, 
+  land_area, 
+  purpose, 
+  description, 
+  year_of_build, 
+  price, 
+  no_of_bathrooms, 
+  no_of_rooms, 
+  no_of_store,
+  no_of_garage
 });
   
 
