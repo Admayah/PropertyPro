febfd39eca38bf42faeb1150087175a482f15374
admaya <mayobami49@gmail.com>
2022-09-01 12:03:06 +0100

    get property data from db to be edited

--- src/components/editProperty/EditProperty.jsx
+++ src/components/editProperty/EditProperty.jsx
@@ -44,7 +44,7 @@ const EditProperty = () => {
       
     }, [])
 
-    const getData = useSelector((state) => state.updateProperty.updateProperty[0])
+    const getData = useSelector((state) => state.updateProperty.updateProperty)
     console.log('store data', getData)
 
 const {  title, address, state, land_area, purpose, description, year_of_build, price, no_of_bathrooms, no_of_rooms, no_of_store, no_of_garage } = getData;
