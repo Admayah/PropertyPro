1d70b221b55c02799069c98ec1314499b5212b29
admaya <mayobami49@gmail.com>
2022-09-01 10:39:56 +0100

    get property data from db to be edited

--- src/store.js
+++ src/store.js
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import propertyReducer from './features/properties/propertiesSlice'
 import adminProperties from './features/properties/adminProperties';
 import userReducer from './features/properties/userSlice';
+import updateReducer from './features/properties/adminProperties';
 // import userReducer from './features/auth/slice';
 
 export const store = configureStore({
   reducer: {
     properties : propertyReducer,
     adminProperties : adminProperties,
+    updateProperty : updateReducer,
     user : userReducer
   },
 })
