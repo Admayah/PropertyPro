c9a9b691f0cb1cffeed0a4e3efb01a7f09208c61
admaya <mayobami49@gmail.com>
2022-09-05 13:52:46 +0100

    working on the dashboard landing page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from 'react-toastify';
 import axios from 'axios'
@@ -19,36 +19,36 @@ export default function Login() {
 
   const dispatch = useDispatch();
 
-    const [user, setUser] = useState(initialValue)
-    const [isDisabled, setDisabled] = useState(false);
+  const [user, setUser] = useState(initialValue)
+  const [isDisabled, setDisabled] = useState(false);
 
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleChange = (e) => {
-      const {name, value} = e.target
-      setUser({...user, [name]: value})
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    setUser({ ...user, [name]: value })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(`${process.env.REACT_APP_BASEURL}/login`, { ...user });
+      const { token } = response.data;
+      localStorage.setItem("token", token);
+      dispatch(
+        addUser({
+          id: new Date().getTime().toString(36),
+          ...user,
+        })
+      );
+      toast('User logged in successfully')
+      setTimeout(navigate("/dashboard"), 10000)
+    } catch (error) {
+      toast.error(`${error.response.data.message}`)
+
     }
-
-    const handleSubmit = async (e) => {
-      e.preventDefault();
-        try {
-          const response = await axios.post(`${process.env.REACT_APP_BASEURL}/login`, {...user});
-          const { token } = response.data;
-          localStorage.setItem("token", token);
-          dispatch(
-            addUser({
-              id: new Date().getTime().toString(36),
-              ...user,
-            })
-          );
-          toast('User logged in successfully')
-          setTimeout(navigate("/dashboard"), 10000)
-        } catch (error) {
-          toast.error(`${error.response.data.message}`)
-
-        }
-    }
+  }
 
   return (
     <>
@@ -60,31 +60,32 @@ export default function Login() {
             <div className="login-header">
               <h2 className="login-header-text">LOGIN</h2>
             </div>
-            <div className="login-data-input-container">
-              <InputField
-              type='email'
-              name='email'
-              value={user.email}
-              placeholder='Enter email address'
-              onChange={handleChange}
-              />
-              <InputField
-              type='password'
-              name='password'
-              value={user.password}
-              placeholder='Enter password'
-              onChange={handleChange}
-              />
-            </div>
-              <Link to='#'><button className="login-button" onClick={handleSubmit}>
-                
+            <form onSubmit={handleSubmit}>
+              <div className="login-data-input-container">
+                <InputField
+                  type='email'
+                  name='email'
+                  value={user.email}
+                  placeholder='Enter email address'
+                  onChange={handleChange}
+                />
+                <InputField
+                  type='password'
+                  name='password'
+                  value={user.password}
+                  placeholder='Enter password'
+                  onChange={handleChange}
+                />
+              </div>
+              <button className="login-button" onClick={handleSubmit}>
                 {isDisabled ? <i class="fa fa-circle-o-notch fa-spin"></i> : 'Login'}
-
-              </button></Link>
+              </button>
+            </form>
+            {/* <Link to='#'></Link> */}
           </div>
 
           <div className="register-link">
-            Don't have an account? &nbsp; <Link to = "/signup">Signup Here.</Link>
+            Don't have an account? &nbsp; <Link to="/signup">Signup Here.</Link>
           </div>
         </div>
       </div>
