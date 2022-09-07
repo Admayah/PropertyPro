51b11f4f9128468e244898ce9fa6fd46bc91ed20
admaya <mayobami49@gmail.com>
2022-09-07 13:31:47 +0100

    made changes to dashboard page

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -7,17 +7,21 @@ function Sidebar() {
   return (
     <div className="sidebar-container">
       <div className="sidebar-wrapper">
+        <div className="company-logo">
+          <i className="fa fa-home logo"></i>
+          <span className="logo-name">9jaProperty</span>
+        </div>
         <ul className="sidenav__list">
           {SidebarData.map((item, index) => (
-              <Link to={item.href} className="sidebar-links">
-            <li className="sidenav__list-item" key={index}>
+            <Link to={item.href} className="sidebar-links">
+              <li className="sidenav__list-item" key={index}>
                 <span className="sidebar-text">
                   <span className="sidebar-icon">
                     <img src={item.icon} alt="" className="sidebar-img" />
                   </span>
                   {item.text}
                 </span>
-            </li>
+              </li>
             </Link>
           ))}
         </ul>
